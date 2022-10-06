d894f1499fc12b2f415231b800aa0c33b8dcac79
Melissa Breese <melissab495@hotmail.com>
2022-10-06 00:20:14 -0600

    using System;
    namespace MyApplication
    {
      class Program
      {
        static void Main(string[] args)
        {
          string name = "Hello, my name is Melissa";
          Console.WriteLine(name);
          Console.Write("Welcome to my page! ");
          Console.Write("I am 26 years old. ");
          Console.Write("I just discovered you can use console to write a whole paragraph without pressing enter all the time if you wanted. ");
          }
          }}

---
+++ Arrays-And-Loops.cs/Access-Elements-Of-Arrays-2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] characters = {"Deku", "Bakugo", "Todoroki", "Ida"};
+      Console.WriteLine(characters[0]);
+    }
+  }
+}
