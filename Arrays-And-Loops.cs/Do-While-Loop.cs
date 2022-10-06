ef37364871aeaf4528976b69b36c7b515d3931ce
Melissa Breese <melissab495@hotmail.com>
2022-10-06 00:17:23 -0600

    using System;
    namespace MyApplication
    {
      class Program
      {
        static void Main(string[] args)
        {
          string name = "Hello, my name is Melissa";
          Console.WriteLine(name);
          Console.Write("Welcome to my page! ");
          Console.Write("I am 26 years old. ");
          Console.Write("I just discovered you can use console to write a whole paragraph without pressing enter all the time if you wanted. ");
          }
          }}

---
+++ Arrays-And-Loops.cs/Do-While-Loop.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 0;
+      while (i < 40)
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+    }
+  }
+}
