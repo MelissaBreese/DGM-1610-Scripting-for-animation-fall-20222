5b64db394f0c8b88f4a4691a9b8f6002ea0a246e
Melissa Breese <melissab495@hotmail.com>
2022-10-07 15:35:15 -0600

    Unit-1c.cs
    2nd attempt at adding files For-Loops_1&2

---
+++ Unit-1c.cs/For-Loops_1-Lab.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 20; i++) 
+      {
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
