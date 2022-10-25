a888e4ebbc60f26b13f7f83f18f06f686058d561
Melissa Breese <melissab495@hotmail.com>
2022-10-25 02:03:44 -0600

    Added Main Player Script

---
+++ Unity/Unit2-Unity-22/Assets/Scenes/MainPlayerScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayerScript : MonoBehaviour  {
+
+   //  Use this for initialization
+   void start ()  {
+   }
+
+   // Update is called once per frame
+   void Update ()  {
+        }
+   }
