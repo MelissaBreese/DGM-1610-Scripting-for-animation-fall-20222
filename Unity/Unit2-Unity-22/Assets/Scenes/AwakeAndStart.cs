7bbc024a3f4c5c580a2c25b1b951ebd36d427fcf
Melissa Breese <melissab495@hotmail.com>
2022-10-27 02:00:17 -0600

    Added Awake and Start

---
+++ Unity/Unit2-Unity-22/Assets/Scenes/AwakeAndStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void awake ()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+
+    void start ()
+    {
+        Debug.Log("Start called.");
+    }
