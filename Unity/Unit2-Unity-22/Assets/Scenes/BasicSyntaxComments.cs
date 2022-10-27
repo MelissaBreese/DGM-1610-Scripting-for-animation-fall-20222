339d165ea9365d348a5d0e16bc481719136b7d04
Melissa Breese <melissab495@hotmail.com>
2022-10-27 03:09:00 -0600

    Comments

---
+++ Unity/Unit2-Unity-22/Assets/Scenes/BasicSyntaxComments.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntaxComments : MonoBehaviour
+{
+    void start()
+    {
+         // I am learning about how to do the comments part of the syntax
+
+         /* Hello, this is an experiement!
+         * This is two lines!
+        **/
+        Debug.Log(transform.position.y);
+
+        if (transform.position.y  <=5f)
+         {
+              Debug.Log("I'm flying!");
+         }
+    }
+}
